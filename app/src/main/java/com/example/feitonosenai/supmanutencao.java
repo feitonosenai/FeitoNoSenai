7dfcf673564ff8829649e19d995057600926d823
Paulo Ferreira <feitonosenai@gmail.com>
2022-06-07 19:33:49 -0300

    0.0.13

--- app/src/main/java/com/example/feitonosenai/supmanutencao.java
+++ app/src/main/java/com/example/feitonosenai/supmanutencao.java
@@ -14,19 +14,25 @@ public class supmanutencao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supmanutencao);
+
+        // Pega os botões da tela
         abrir_pedido_material = (Button) findViewById(R.id.btnAbriPedidoMaterial);
         clasificar = (Button) findViewById(R.id.btnCasifica);
         sair = (Button) findViewById(R.id.btnSair1);
+
+        // Configura suas ações OnClick
         abrir_pedido_material.setOnClickListener(this::novoPedidoMaterial);
         clasificar.setOnClickListener(this::clasifica);
         sair.setOnClickListener(this::sai);
     }
     private void novoPedidoMaterial (View view) {
-        Intent intencao = new Intent(this, abripedidomaterial.class); // subistituir CadastroActivity pela activity da pagina de abrir pedido material
+        // Encaminha para abertura de pedido de material
+        Intent intencao = new Intent(this, abripedidomaterial.class);
         startActivity(intencao);
     }
     private void clasifica (View view) {
-        Intent intencao = new Intent(this, CadastroActivity.class); // subistituir CadastroActivity pela activity da pagina de clasificaçao
+        // Encaminha para a tela de classificação de pedidos
+        Intent intencao = new Intent(this, ClassificacaoActivity.class);
         startActivity(intencao);
     }
     private void sai (View view) {
